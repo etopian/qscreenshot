6d2cc14795bf5d367effce9b8173340efe22e566
vladimir.shelukhin <vladimir.shelukhin@47345aec-9937-dfb8-a2f7-2325e5194605>
2013-09-30 09:25:02 +0000

    * updated win32 build script
    git-svn-id: https://qscreenshot.googlecode.com/svn/trunk@140 47345aec-9937-dfb8-a2f7-2325e5194605

--- scripts/win/make-qscreenshot-and-upload-to-googlecode.cmd
+++ scripts/win/make-qscreenshot-and-upload-to-googlecode.cmd
@@ -9,13 +9,14 @@ configure
 mingw32-make -f makefile
 @echo Finish time: %TIME%>>..\..\timestamps.log
 move /Y src\release\qscreenshot.exe ..\..\qscreenshot.exe
+lrelease src/src.pro
 cd ..\..
 pause
 @echo Archiving qScreenshot Nightly Build
-call 7z a -mx9 "qscreenshot-r136-win32.7z" "qscreenshot.exe"
+call 7z a -mx9 "qscreenshot-r139-win32.7z" "qscreenshot.exe"
 @echo Completed
 pause
 @echo Uploading archived qScreenshot Nightly Build to Google Code
-call googlecode_upload.py -p "qscreenshot" -s "qScreenshot Nightly Build || Qt 4.8.5 || For test only!" -l "NightlyBuild,Windows,Archive" "qscreenshot-r136-win32.7z"
+call googlecode_upload.py -p "qscreenshot" -s "qScreenshot Nightly Build || Qt 4.8.5 || For test only!" -l "NightlyBuild,Windows,Archive" "qscreenshot-r139-win32.7z"
 @echo Completed
 pause & pause
