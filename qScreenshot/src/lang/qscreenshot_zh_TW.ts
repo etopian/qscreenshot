7135a6345b09f058aeb970ca36510a957c686271
wadealer <wadealer@gmail.com>
2014-08-11 09:44:23 +0000

    new translation, thx chuanjang

---
+++ qScreenshot/src/lang/qscreenshot_zh_TW.ts
@@ -0,0 +1,622 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW">
+<context>
+    <name>AboutDlg</name>
+    <message>
+        <source>About qScreenshot</source>
+        <translation>關於 qScreenshot</translation>
+    </message>
+    <message>
+        <source>qScreenshot</source>
+        <translation>qScreenshot</translation>
+    </message>
+    <message>
+        <source>Simple creation and editing of screenshots</source>
+        <translation>簡易建立、編輯螢幕擷圖</translation>
+    </message>
+    <message>
+        <source>&lt;a href=&quot;http://code.google.com/p/qscreenshot/&quot;&gt;qScreenshot&apos;s home page</source>
+        <translation>&lt;a href=&quot;http://code.google.com/p/qscreenshot/&quot;&gt;qScreenshot 官網</translation>
+    </message>
+</context>
+<context>
+    <name>Controller</name>
+    <message>
+        <source>Error</source>
+        <translation>錯誤</translation>
+    </message>
+    <message>
+        <source>Failed to register shortcut!</source>
+        <translation>無法註冊快速鍵!</translation>
+    </message>
+    <message>
+        <source>Grab screen</source>
+        <translation>擷取螢幕</translation>
+    </message>
+    <message>
+        <source>New Screenshot</source>
+        <translation>新的螢幕擷圖</translation>
+    </message>
+    <message>
+        <source>Open Image</source>
+        <translation>開啟影像</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>設定</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>選項</translation>
+    </message>
+    <message>
+        <source>Proxy settings</source>
+        <translation>Proxy 設定</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>語言</translation>
+    </message>
+    <message>
+        <source>Translate</source>
+        <translation>翻譯</translation>
+    </message>
+    <message>
+        <source>Restart application!</source>
+        <translation>重新啟動應用程式!</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>結束</translation>
+    </message>
+    <message>
+        <source>Screenshot saved.</source>
+        <translation>螢幕擷圖存檔.</translation>
+    </message>
+    <message>
+        <source>Grab active window</source>
+        <translation>擷取使用中視窗</translation>
+    </message>
+    <message>
+        <source>Grab area</source>
+        <translation>擷取區域</translation>
+    </message>
+</context>
+<context>
+    <name>EditServerDlg</name>
+    <message>
+        <source>Server Settings</source>
+        <translation>伺服器設定</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>名稱:</translation>
+    </message>
+    <message>
+        <source>Url:</source>
+        <translation>URL:</translation>
+    </message>
+    <message>
+        <source>User Name*:</source>
+        <translation>使用者名稱*:</translation>
+    </message>
+    <message>
+        <source>Password*:</source>
+        <translation>密碼*:</translation>
+    </message>
+    <message>
+        <source>PostData**:</source>
+        <translation>PostData**:</translation>
+    </message>
+    <message>
+        <source>File Input**:</source>
+        <translation>檔案輸入**:</translation>
+    </message>
+    <message>
+        <source>RegExp**:</source>
+        <translation>RegExp**:</translation>
+    </message>
+    <message>
+        <source>*for FTP servers only</source>
+        <translation>*僅 FTP 伺服器適用</translation>
+    </message>
+    <message>
+        <source>**for HTTP servers only</source>
+        <translation>**僅 HTTP 伺服器適用</translation>
+    </message>
+    <message>
+        <source>Use proxy server</source>
+        <translation>使用 proxy 伺服器</translation>
+    </message>
+</context>
+<context>
+    <name>GetTextDlg</name>
+    <message>
+        <source>Enter text</source>
+        <translation>輸入文字</translation>
+    </message>
+    <message>
+        <source>Select Font</source>
+        <translation>選擇字型</translation>
+    </message>
+</context>
+<context>
+    <name>GrabAreaWidget</name>
+    <message>
+        <source>Select area</source>
+        <translation>選取區域</translation>
+    </message>
+</context>
+<context>
+    <name>GrepShortcutKeyDialog</name>
+    <message>
+        <source>New Shortcut</source>
+        <translation>新的快速鍵方式</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <source>Set Keys</source>
+        <translation>設定快速鍵</translation>
+    </message>
+</context>
+<context>
+    <name>HistoryDlg</name>
+    <message>
+        <source>History</source>
+        <translation>歷程記錄</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>複製</translation>
+    </message>
+    <message>
+        <source>Copy link to the clipboard</source>
+        <translation>將連結複製到剪貼簿</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>開啟</translation>
+    </message>
+    <message>
+        <source>Open link in browser</source>
+        <translation>於瀏覽器中開啟連結</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>關閉</translation>
+    </message>
+    <message>
+        <source>Close history</source>
+        <translation>關閉歷程記錄</translation>
+    </message>
+</context>
+<context>
+    <name>OptionsDlg</name>
+    <message>
+        <source>Settings</source>
+        <translation>設定</translation>
+    </message>
+</context>
+<context>
+    <name>OptionsWidget</name>
+    <message>
+        <source>Form</source>
+        <translation>表單</translation>
+    </message>
+    <message>
+        <source>Automatically launch application when OS starts</source>
+        <translation>當作業系統啟動時自動啟動應用程式</translation>
+    </message>
+    <message>
+        <source>Shortcut:</source>
+        <translation>快速鍵組合方式:</translation>
+    </message>
+    <message>
+        <source>Format:</source>
+        <translation>格式:</translation>
+    </message>
+    <message>
+        <source>File Name:</source>
+        <translation>檔案名稱:</translation>
+    </message>
+    <message>
+        <source>Modify hotkey</source>
+        <translation>修改快速鍵</translation>
+    </message>
+    <message>
+        <source>Modify</source>
+        <translation>修改</translation>
+    </message>
+    <message>
+        <source>Servers:</source>
+        <translation>伺服器:</translation>
+    </message>
+    <message>
+        <source>Add new server</source>
+        <translation>新增伺服器</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>新增</translation>
+    </message>
+    <message>
+        <source>Delete current server</source>
+        <translation>刪除目前伺服器</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>刪除</translation>
+    </message>
+    <message>
+        <source>Edit current server</source>
+        <translation>編輯目前伺服器</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>編輯</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>錯誤</translation>
+    </message>
+    <message>
+        <source>Failed to register shortcut!</source>
+        <translation>無法註冊快速鍵!</translation>
+    </message>
+    <message>
+        <source>Default action</source>
+        <translation>預設動作</translation>
+    </message>
+    <message>
+        <source>Capture the desktop</source>
+        <translation>擷取桌面</translation>
+    </message>
+    <message>
+        <source>Capture active window</source>
+        <translation>擷取使用中視窗</translation>
+    </message>
+    <message>
+        <source>Select capture area</source>
+        <translation>選取擷取區域</translation>
+    </message>
+    <message>
+        <source>*to specify the order of servers, use the drag-n-drop </source>
+        <translation>*若要指定伺服器的順序,使用滑鼠拖放功能</translation>
+    </message>
+    <message>
+        <source>Automatically save screenshots to folder:</source>
+        <translation>自動將螢幕擷圖儲存存到資料夾:</translation>
+    </message>
+    <message>
+        <source>Chose Folder</source>
+        <translation>選擇資料夾</translation>
+    </message>
+    <message>
+        <source>Automatically check for updates on startup</source>
+        <translation>啟動時自動檢查更新</translation>
+    </message>
+    <message>
+        <source>Defaults</source>
+        <translation>預設</translation>
+    </message>
+    <message>
+        <source>Reset Settings</source>
+        <translation>重設設定</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to completely
+reset settings to the default values?
+Application will be restarted.</source>
+        <translation>你確定想完全重設設定為預設值?
+將重新開啟應用程式.</translation>
+    </message>
+</context>
+<context>
+    <name>PixmapWidget</name>
+    <message>
+        <source>Cut</source>
+        <translation>剪下</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>複製</translation>
+    </message>
+    <message>
+        <source>Input radius</source>
+        <translation>輸入半徑</translation>
+    </message>
+    <message>
+        <source>Radius</source>
+        <translation>半徑</translation>
+    </message>
+    <message>
+        <source>Blur</source>
+        <translation>模糊</translation>
+    </message>
+</context>
+<context>
+    <name>ProxySettingsDlg</name>
+    <message>
+        <source>Proxy Server Settings</source>
+        <translation>Proxy 伺服器設定</translation>
+    </message>
+    <message>
+        <source>Type:</source>
+        <translation>類型:</translation>
+    </message>
+    <message>
+        <source>Host:</source>
+        <translation>主機:</translation>
+    </message>
+    <message>
+        <source>Port:</source>
+        <translation>連接埠:</translation>
+    </message>
+    <message>
+        <source>User:</source>
+        <translation>使用者:</translation>
+    </message>
+    <message>
+        <source>Password:</source>
+        <translation>密碼:</translation>
+    </message>
+    <message>
+        <source>99999; </source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Screenshot</name>
+    <message>
+        <source>qScreenshot</source>
+        <translation>qScreenshot</translation>
+    </message>
+    <message>
+        <source>Cancel uploading</source>
+        <translation>取消上傳</translation>
+    </message>
+    <message>
+        <source>Upload to server:</source>
+        <translation>上傳到伺服器:</translation>
+    </message>
+    <message>
+        <source>Upload Screenshot to selected server</source>
+        <translation>上傳螢幕擷圖到選取的伺服器</translation>
+    </message>
+    <message>
+        <source>Save Screenshot to Local Drive</source>
+        <translation>儲存螢幕擷圖到本機磁碟</translation>
+    </message>
+    <message>
+        <source>Print Screenshot</source>
+        <translation>列印螢幕擷圖</translation>
+    </message>
+    <message>
+        <source>Open image from Local Drive</source>
+        <translation>開啟本機磁碟影像</translation>
+    </message>
+    <message>
+        <source>New Screenshot</source>
+        <translation>新增螢幕擷圖</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation>檔案</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>設定</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>說明</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>開啟</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>儲存</translation>
+    </message>
+    <message>
+        <source>Print</source>
+        <translation>列印</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>結束</translation>
+    </message>
+    <message>
+        <source>Proxy Settings</source>
+        <translation>Proxy 設定</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>選項</translation>
+    </message>
+    <message>
+        <source>History</source>
+        <translation>歷程記錄</translation>
+    </message>
+    <message>
+        <source>About Qt</source>
+        <translation>關於 Qt</translation>
+    </message>
+    <message>
+        <source>Home page</source>
+        <translation>qScreenshot 首頁</translation>
+    </message>
+    <message>
+        <source>Upload</source>
+        <translation>上傳</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>關於</translation>
+    </message>
+    <message>
+        <source>Check for updates</source>
+        <translation>檢查更新</translation>
+    </message>
+    <message>
+        <source>Copy to clipboard</source>
+        <translation>複製到剪貼簿</translation>
+    </message>
+    <message>
+        <source>ToolBar</source>
+        <translation>工具列</translation>
+    </message>
+</context>
+<context>
+    <name>ScreenshotMainWin</name>
+    <message>
+        <source>Size: %1x%2px; %3 bytes</source>
+        <translation>大小: %1x%2px; %3 bytes</translation>
+    </message>
+    <message>
+        <source>Open Image</source>
+        <translation>開啟影像</translation>
+    </message>
+    <message>
+        <source>Images (*.png *.gif *.jpg *.jpeg *.ico)</source>
+        <translation>影像 (*.png *.gif *.jpg *.jpeg *.ico)</translation>
+    </message>
+    <message>
+        <source>Save As</source>
+        <translation>另存新檔</translation>
+    </message>
+    <message>
+        <source>%1 Files (*.%2);;All Files (*)</source>
+        <translation>%1 檔案 (*.%2);;所有檔案 (*)</translation>
+    </message>
+    <message>
+        <source>Close Screenshot</source>
+        <translation>關閉螢幕擷圖</translation>
+    </message>
+    <message>
+        <source>Are your sure?</source>
+        <translation>您確定?</translation>
+    </message>
+</context>
+<context>
+    <name>ScreenshotOptions</name>
+    <message>
+        <source>New Screenshot</source>
+        <translation>新增螢幕擷圖</translation>
+    </message>
+    <message>
+        <source>Capture the desktop</source>
+        <translation>擷取桌面</translation>
+    </message>
+    <message>
+        <source>Capture current window</source>
+        <translation>擷取目前視窗</translation>
+    </message>
+    <message>
+        <source>Select capture area</source>
+        <translation>選取擷取區域</translation>
+    </message>
+    <message>
+        <source>Capture after</source>
+        <translation>擷取後</translation>
+    </message>
+    <message>
+        <source>seconds</source>
+        <translation>秒</translation>
+    </message>
+</context>
+<context>
+    <name>ToolBar</name>
+    <message>
+        <source>Line Width:</source>
+        <translation>線條寬度:</translation>
+    </message>
+    <message>
+        <source>Line width</source>
+        <translation>線條寬度</translation>
+    </message>
+    <message>
+        <source>Select Color</source>
+        <translation>選取色彩</translation>
+    </message>
+    <message>
+        <source>Pen</source>
+        <translation>畫筆</translation>
+    </message>
+    <message>
+        <source>Ellipse</source>
+        <translation>橢圓形</translation>
+    </message>
+    <message>
+        <source>Rectangle</source>
+        <translation>矩形</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>選取</translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation>剪下</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>複製</translation>
+    </message>
+    <message>
+        <source>Insert</source>
+        <translation>插入</translation>
+    </message>
+    <message>
+        <source>Rotate</source>
+        <translation>旋轉</translation>
+    </message>
+    <message>
+        <source>Insert Text</source>
+        <translation>插入文字</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation>復原</translation>
+    </message>
+    <message>
+        <source>Blur</source>
+        <translation>模糊</translation>
+    </message>
+</context>
+<context>
+    <name>UpdatesChecker</name>
+    <message>
+        <source>Error while checking for updates: %1</source>
+        <translation>檢查更新時出錯: %1</translation>
+    </message>
+    <message>
+        <source>New version is available</source>
+        <translation>有新版本可用</translation>
+    </message>
+    <message>
+        <source>Do you want to go to the download page?</source>
+        <translation>你想要連結到下載頁面嗎?</translation>
+    </message>
+    <message>
+        <source>There is no updates found.</source>
+        <translation>沒有發現新版本.</translation>
+    </message>
+    <message>
+        <source>Can&apos;t find information about version.</source>
+        <translation>找不到版本資訊.</translation>
+    </message>
+</context>
+<context>
+    <name>Uploader</name>
+    <message>
+        <source>Can&apos;t parse URL (Reply URL: &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;)</source>
+        <translation>無法剖析 URL (回覆 URL: &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;)</translation>
+    </message>
+</context>
+</TS>
